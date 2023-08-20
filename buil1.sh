91e652c37f2f6092c85ae3495eec79a3ea7c6f7b
maheleet <leetmahe@gmail.com>
2023-08-20 12:44:29 +0530

    sh script updated

--- buil1.sh
+++ buil1.sh
@@ -8,19 +8,10 @@ new_tag=$((current_tag + 1))
 
 # Build the Docker image with the new tag
 if docker build -t ubuntu:${new_tag} .; then
-  # Prompt user to proceed
-  read -p "Image build successful. Do you want to run 'docker-compose up -d'? (yes/no): " choice
-  if [ "$choice" == "yes" ]; then
-    # Update the Docker Compose file with the new image tag
     sed -i "s|image: ubuntu:.\\+|image: ubuntu:${new_tag}|" docker-compose.yml
     
-    # Run docker-compose up -d
-    docker-compose up -d
-    echo "Docker image built and container updated with new image: ubuntu:${new_tag}"
+    echo "Docker image built and image name updated with new image: ubuntu:${new_tag}"
   else
-    echo "Docker image built with new tag: ${new_tag}, but 'docker-compose up -d' not executed."
-  fi
-else
   echo "Image build failed. Docker image not created."
 fi
 
